24c7679f2d38fe1d4f3af2b8985c568523f97ef6
pietro <battilanap@gmail.com>
2018-08-13 12:16:29 +0200

    basic single and multi wctransforms structure

--- autoencoderWCT.py
+++ autoencoderWCT.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+from wct import wctransform
 from torch.utils.serialization import load_lua
 from encoder_decoder_VGG19 import Encoder, Decoder1, Decoder2, Decoder3, Decoder4, Decoder5
 
@@ -6,6 +7,8 @@ class SingleLevelWCT(nn.Module):
 
     def __init__(self, args):
         super(SingleLevelWCT, self).__init__()
+
+        self.alpha = args.alpha
 
         e5_vgg5 = load_lua(args.encoder5_vgg19)
         self.e5 = Encoder(e5_vgg5, 5)
@@ -15,61 +18,13 @@ class SingleLevelWCT(nn.Module):
 
 
     def forward(self, content_img, style_img):
-        out = self.e5(content_img)
 
-        # def whiten_and_color(self,cF,sF):
-        #     cFSize = cF.size()
-        #     c_mean = torch.mean(cF,1) # c x (h x w)
-        #     c_mean = c_mean.unsqueeze(1).expand_as(cF)
-        #     cF = cF - c_mean
-        #
-        #     contentConv = torch.mm(cF,cF.t()).div(cFSize[1]-1) + torch.eye(cFSize[0]).double()
-        #     c_u,c_e,c_v = torch.svd(contentConv,some=False)
-        #
-        #     k_c = cFSize[0]
-        #     for i in range(cFSize[0]):
-        #         if c_e[i] < 0.00001:
-        #             k_c = i
-        #             break
-        #
-        #     sFSize = sF.size()
-        #     s_mean = torch.mean(sF,1)
-        #     sF = sF - s_mean.unsqueeze(1).expand_as(sF)
-        #     styleConv = torch.mm(sF,sF.t()).div(sFSize[1]-1)
-        #     s_u,s_e,s_v = torch.svd(styleConv,some=False)
-        #
-        #     k_s = sFSize[0]
-        #     for i in range(sFSize[0]):
-        #         if s_e[i] < 0.00001:
-        #             k_s = i
-        #             break
-        #
-        #     c_d = (c_e[0:k_c]).pow(-0.5)
-        #     step1 = torch.mm(c_v[:,0:k_c],torch.diag(c_d))
-        #     step2 = torch.mm(step1,(c_v[:,0:k_c].t()))
-        #     whiten_cF = torch.mm(step2,cF)
-        #
-        #     s_d = (s_e[0:k_s]).pow(0.5)
-        #     targetFeature = torch.mm(torch.mm(torch.mm(s_v[:,0:k_s],torch.diag(s_d)),(s_v[:,0:k_s].t())),whiten_cF)
-        #     targetFeature = targetFeature + s_mean.unsqueeze(1).expand_as(targetFeature)
-        #     return targetFeature
-        #
-        # def transform(self,cF,sF,csF,alpha):
-        #     cF = cF.double()
-        #     sF = sF.double()
-        #     C,W,H = cF.size(0),cF.size(1),cF.size(2)
-        #     _,W1,H1 = sF.size(0),sF.size(1),sF.size(2)
-        #     cFView = cF.view(C,-1)
-        #     sFView = sF.view(C,-1)
-        #
-        #     targetFeature = self.whiten_and_color(cFView,sFView)
-        #     targetFeature = targetFeature.view_as(cF)
-        #     ccsF = alpha * targetFeature + (1.0 - alpha) * cF
-        #     ccsF = ccsF.float().unsqueeze(0)
-        #     csF.data.resize_(ccsF.size()).copy_(ccsF)
-        #     return csF
+        cf = self.e5(content_img).data.cpu().squeeze(0)
+        sf = self.e5(style_img).data.cpu().squeeze(0)
 
-        out = self.d5(out)
+        outf = wctransform(self.alpha, cf, sf)
+        out = self.d5(outf)
+
         return out
 
 
@@ -77,6 +32,8 @@ class MultiLevelWCT(nn.Module):
 
     def __init__(self, args):
         super(MultiLevelWCT, self).__init__()
+
+        self.alpha = args.alpha
 
         e1_vgg1 = load_lua(args.encoder1_vgg19)
         e2_vgg2 = load_lua(args.encoder2_vgg19)
@@ -104,7 +61,31 @@ class MultiLevelWCT(nn.Module):
 
 
     def forward(self, content_img, style_img):
-        out = None
 
+        cf5 = self.e5(content_img).data.cpu().squeeze(0)
+        sf5 = self.e5(style_img).data.cpu().squeeze(0)
+        csf5 = wctransform(self.alpha, cf5, sf5)
+        im5 = self.d5(csf5)
 
+        cf4 = self.e4(im5).data.cpu().squeeze(0)
+        sf4 = self.e4(style_img).data.cpu().squeeze(0)
+        csf4 = wctransform(self.alpha, cf4, sf4)
+        im4 = self.d4(csf4)
+
+        cf3 = self.e3(im4).data.cpu().squeeze(0)
+        sf3 = self.e3(style_img).data.cpu().squeeze(0)
+        csf3 = wctransform(self.alpha, cf3, sf3)
+        im3 = self.d3(csf3)
+
+        cf2 = self.e2(im3).data.cpu().squeeze(0)
+        sf2 = self.e2(style_img).data.cpu().squeeze(0)
+        csf2 = wctransform(self.alpha, cf2, sf2)
+        im2 = self.d2(csf2)
+
+        cf1 = self.e1(im2).data.cpu().squeeze(0)
+        sf1 = self.e1(style_img).data.cpu().squeeze(0)
+        csf1 = wctransform(self.alpha, cf1, sf1)
+        im1 = self.d5(csf1)
+
+        out = im1
         return out
---
+++ wct.py
@@ -0,0 +1,55 @@
+import torch
+
+def wctransform(alpha, cf, sf, outf=None):
+
+    cf = cf.double()
+    sf = sf.double()
+    C,W,H = cf.size(0), cf.size(1), cf.size(2)
+    _,W1,H1 = sf.size(0), sf.size(1), sf.size(2)
+    cFView = cf.view(C, -1)
+    sFView = sf.view(C, -1)
+
+
+
+    cFSize = cf.size()
+    c_mean = torch.mean(cf, 1) # c x (h x w)
+    c_mean = c_mean.unsqueeze(1).expand_as(cf)
+    cf = cf - c_mean
+
+    contentConv = torch.mm(cf, cf.t()).div(cFSize[1] - 1) + torch.eye(cFSize[0]).double()
+    c_u,c_e,c_v = torch.svd(contentConv,some=False)
+
+    k_c = cFSize[0]
+    for i in range(cFSize[0]):
+        if c_e[i] < 0.00001:
+            k_c = i
+            break
+
+    sFSize = sf.size()
+    s_mean = torch.mean(sf, 1)
+    sf = sf - s_mean.unsqueeze(1).expand_as(sf)
+    styleConv = torch.mm(sf, sf.t()).div(sFSize[1] - 1)
+    s_u,s_e,s_v = torch.svd(styleConv,some=False)
+
+    k_s = sFSize[0]
+    for i in range(sFSize[0]):
+        if s_e[i] < 0.00001:
+            k_s = i
+            break
+
+    c_d = (c_e[0:k_c]).pow(-0.5)
+    step1 = torch.mm(c_v[:,0:k_c],torch.diag(c_d))
+    step2 = torch.mm(step1,(c_v[:,0:k_c].t()))
+    whiten_cF = torch.mm(step2, cf)
+
+    s_d = (s_e[0:k_s]).pow(0.5)
+    targetFeature = torch.mm(torch.mm(torch.mm(s_v[:,0:k_s],torch.diag(s_d)),(s_v[:,0:k_s].t())),whiten_cF)
+    targetFeature = targetFeature + s_mean.unsqueeze(1).expand_as(targetFeature)
+
+
+    targetFeature = targetFeature.view_as(cf)
+    ccsF = alpha * targetFeature + (1.0 - alpha) * cf
+    ccsF = ccsF.float().unsqueeze(0)
+    outf.data.resize_(ccsF.size()).copy_(ccsF)
+
+    return outf
